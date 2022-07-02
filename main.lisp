f0dae58baaa762f8a293c4a81f548b94faf2d9cf
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-07-02 11:16:48 +0200

    Add files via upload

--- main.lisp
+++ main.lisp
@@ -1,18 +1,32 @@
 #!/usr/bin/sbcl --script
+(require "asdf")
 
-;; Enable quicklisp
+
 (defun load-quicklisp ()
+  "Initialize quicklisp."
   #-quicklisp
   (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                          (user-homedir-pathname))))
     (when (probe-file quicklisp-init)
       (load quicklisp-init))))
 
-(require "asdf")
-
+(defun replace-substring (string part replacement &key (test #'char=))
+  "Returns a new string in which all the occurences of the part
+is replaced with replacement."
+  (with-output-to-string (out)
+    (loop with part-length = (length part)
+          for old-pos = 0 then (+ pos part-length)
+          for pos = (search part string
+                            :start2 old-pos
+                            :test test)
+          do (write-string string out
+                           :start old-pos
+                           :end (or pos (length string)))
+          when pos do (write-string replacement out)
+          while pos)))
 
 (defun get-cmd-output (cmd)
-  "get output of command as a string"
+  "Returns output of shell command as a string."
   (let ((fstr (make-array '(0) :element-type 'base-char
                                :fill-pointer 0 :adjustable t)))
     (with-output-to-string (s fstr)
@@ -23,7 +37,7 @@
   (uiop:run-program cmd))
 
 (defun get-argv-string ()
-  "get command line arguments as a string"
+  "Returns command line arguments as a space-separated string."
   (let ((fstr (make-array '(0) :element-type 'base-char
                                :fill-pointer 0 :adjustable t)))
     (with-output-to-string (s fstr)
@@ -32,3 +46,4 @@
 
 
 (format t "~a~%" (get-argv-string))
+(format t "~a~%" (cdr *posix-argv*))
